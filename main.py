500783b174f104be8af6a65457fbc10a18eb5a42
kamsingh2050 <kamaljit.singh@stonybrook.edu>
2023-05-15 03:10:40 +0000

    just making sure everything is there

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from flask import Flask
 import pandas as pd
 
-df = pd.read_csv('./data/smallUtilization.csv')
+df = pd.read_csv('./data/utilization2019.csv')
 
 app = Flask(__name__)
 
 @app.route('/', methods = ["GET"])
-
 def home():
-    return "This is an API service for MN ICD code details"
+    return "This is an API service for MN ICD Code details"
 
-app.route('/preview', methods = ["GET"])
+
+@app.route('/preview', methods = ["GET"])
+
 def preview():
     top10rows = df.head(1)
     result = top10rows.to_json(orient="records")
